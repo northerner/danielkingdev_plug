57b0c8ddda19908662852abfc23c57fadecb3b88
Daniel King <dan@kin.gy>
2023-03-05 23:45:36 +0000

    Remove NimblePublisher

--- mix.exs
+++ mix.exs
@@ -23,12 +23,8 @@ defmodule DanielkingdevPlug.MixProject do
   defp deps do
     [
       {:bandit, ">= 0.6.8"},
-      {:nimble_publisher, "~> 0.1.0"},
-      {:makeup_elixir, ">= 0.0.0"},
-      {:makeup_erlang, ">= 0.0.0"},
-      {:atomex, "0.3.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:atomex, "0.3.0"},
+      {:earmark, "~> 1.4"}
     ]
   end
 end
