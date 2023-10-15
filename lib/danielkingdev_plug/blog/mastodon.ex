97dda016d90d709c023e6ebd334b4386790d955a
Daniel King <dan@kin.gy>
2023-10-15 14:24:26 +0100

    Fix mastodon status display and cache

--- lib/danielkingdev_plug/blog/mastodon.ex
+++ lib/danielkingdev_plug/blog/mastodon.ex
@@ -2,17 +2,16 @@ defmodule DanielkingdevPlug.Blog.Mastodon do
   use Agent
 
   def start_link(_) do
-    initial_statuses = fetch()
-    Agent.start_link(fn -> {NaiveDateTime.utc_now(), initial_statuses} end, name: __MODULE__)
+    Agent.start_link(fn -> {NaiveDateTime.utc_now(), []} end, name: __MODULE__)
   end
 
   def statuses do
     {time, statuses} = Agent.get(__MODULE__, &(&1))
     beginning_of_today = NaiveDateTime.beginning_of_day(NaiveDateTime.utc_now())
 
-    if (NaiveDateTime.before?(time, beginning_of_today)) do
+    if statuses == [] || (NaiveDateTime.before?(time, beginning_of_today)) do
       new_statuses = fetch()
-      Agent.update(__MODULE__, fn _ -> {NaiveDateTime.utc_now, new_statuses} end)
+      Agent.update(__MODULE__, fn _ -> {NaiveDateTime.utc_now(), new_statuses} end)
       new_statuses
     else
       statuses
@@ -28,6 +27,6 @@ defmodule DanielkingdevPlug.Blog.Mastodon do
       cache: true
     )
 
-    Enum.map(resp.body, fn item -> Map.take(item, ["content", "created_at", "url"]) end)
+    Enum.map(resp.body, fn item -> Map.take(item, ["content", "created_at", "url", "media_attachments"]) end)
   end
 end
